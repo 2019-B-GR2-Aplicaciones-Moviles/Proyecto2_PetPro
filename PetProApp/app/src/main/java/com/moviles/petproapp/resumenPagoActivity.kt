aa5a731c9376bbf285b54d1ce17d3203f4c8de16
MishellReal <mishell.real@epn.edu.ec>
2020-02-11 02:35:20 -0500

    Cambio de nombre a pantalla pago en efectivo por resumen de pago - Asignar funcionalidad a botones en metodo de pago

---
+++ PetProApp/app/src/main/java/com/moviles/petproapp/resumenPagoActivity.kt
@@ -0,0 +1,23 @@
+package com.moviles.petproapp
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class resumenPagoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resumen_pago)
+
+
+
+        val button: Button = findViewById(R.id.buttonAceptarPagoEfectivo)
+        button.setOnClickListener {
+            var prIntent: Intent = Intent(this, HomeActivity::class.java)
+            startActivity(prIntent)
+        }
+    }
+
+
+}
