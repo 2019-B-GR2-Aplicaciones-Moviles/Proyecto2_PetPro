2f9a6ff3830912dcb600c1f69440d70e0f0729d4
JavierTapia96 <javier.tapia@epn.edu.ec>
2020-02-10 22:35:50 -0500

    Registrar mascota en firebase del usuario

--- PetProApp/app/src/main/java/com/moviles/petproapp/RegistryPetActivity.kt
+++ PetProApp/app/src/main/java/com/moviles/petproapp/RegistryPetActivity.kt
@@ -1,46 +1,54 @@
 package com.moviles.petproapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.Toast
-import com.google.firebase.auth.FirebaseAuth
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_registry_pet.*
 
 class RegistryPetActivity : AppCompatActivity() {
 
     private lateinit var nombreMascota: EditText
-    private lateinit var edadMacota: EditText
+    private lateinit var edadMascota: EditText
     private lateinit var descripcionMascota: EditText
-    private lateinit var vacunaMascota: CheckBox
+    private lateinit var radioGroupEspecie: RadioGroup
+    private lateinit var radioButtonEspecie: RadioButton
+    private var selectId: Int = 0
     private lateinit var nombre: String
-    private lateinit var edad: String
+    private var edad: Int = 0
     private lateinit var descripcion: String
+    private lateinit var especie: String
     private var vacuna: Boolean = false
-
+    private var id: String = ""
+    private lateinit var db: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registry_pet)
 
+        db = FirebaseFirestore.getInstance()
+        id = intent.getStringExtra("id")
+        radioGroupEspecie = findViewById(R.id.radioGroupEspecie)
+        selectId = radioGroupEspecie.checkedRadioButtonId
+        radioButtonEspecie = findViewById(selectId)
+        especie = radioButtonEspecie.text.toString()
         nombreMascota = findViewById(R.id.editTextNombreMascota)
-        edadMacota = findViewById(R.id.editTextEdadMascota)
+        edadMascota = findViewById(R.id.editTextEdadMascota)
         descripcionMascota = findViewById(R.id.editTextDescripcionMascota)
-        checkBoxVacunaAlDia.setOnClickListener(View.OnClickListener {
+        checkBoxVacunaAlDia.setOnClickListener {
             vacuna = checkBoxVacunaAlDia.isChecked
-        })
+        }
+
     }
 
     fun onClickButtonFinRegistroMascota(view: View) {
-        val prIntent = Intent(this, LoginActivity::class.java)
-        startActivity(prIntent)
-
         nombre = nombreMascota.text.toString()
-        edad = edadMacota.text.toString()
+        edad = edadMascota.text.toString().toInt()
         descripcion = descripcionMascota.text.toString()
 
 
@@ -48,23 +56,41 @@ class RegistryPetActivity : AppCompatActivity() {
             nombre.isEmpty() -> {
                 nombreMascota.error = "Campo obligatorio"
             }
-            edad.isEmpty() -> {
-                edadMacota.error = "Campo obligatorio"
+            edad == 0 -> {
+                edadMascota.error = "Campo obligatorio"
             }
-
             descripcion.isEmpty() -> {
                 descripcionMascota.error = "Campo obligatorio"
             }
-
+            radioGroupEspecie.checkedRadioButtonId == -1 -> {
+                Toast.makeText(this, "Debe selecccionar una especie", Toast.LENGTH_LONG).show()
+            }
             else -> {
+                val pet = hashMapOf(
+                    "especie" to especie,
+                    "nombre" to nombre,
+                    "edad" to edad,
+                    "vacunas" to vacuna,
+                    "descripcion" to descripcion
+                )
 
+                sendToFirestore(pet)
             }
-            }
+        }
 
     }
 
-
-
-
-
+    private fun sendToFirestore(pet: HashMap<String, Any>) {
+        db.collection("usuarios").document(id).collection("mascotas")
+            .add(pet as Map<String, Any>)
+            .addOnSuccessListener { documentReference ->
+                // Toast.makeText(this,"DocumentSnapshot added with ID: " + documentReference.id,Toast.LENGTH_LONG).show()
+                val prIntent = Intent(this, HomeActivity::class.java)
+                startActivity(prIntent)
+                finish()
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(this, "Error durante registro", Toast.LENGTH_LONG).show()
+            }
+    }
 }
